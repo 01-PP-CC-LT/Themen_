f093a4945a56e8a1b321ede45b203e6e6c819931
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-05-14 09:44:10 +0000

    rechner | division 1. Iteration

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -11,6 +11,23 @@
 */
 
 // module: multiplication a * b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+function divide(a,b) {
+
+	if (b !== 0) {
+		return a / b;
+	} else {
+		return "Error: Divided by 0!";
+	}
+   
+}
+
+// module: multiplication a * b |  test:
 // output(multiply(3,2));
 // output(multiply(3,-2));
 // output(multiply(3,0));
